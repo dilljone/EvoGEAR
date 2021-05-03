74635a1389dbe1de2493e889737e261d7d535c45
Dillon Jones <dilljone96@gmail.com>
2021-05-03 13:36:58 -0700

    Added functions found in other projects to functions.r. Functions largley related to Allopatry pipeline; Added allopatry script which still needs to be functionalized, tested, and turned into something ~actually~ useful

---
+++ R/Allopatry.R
@@ -0,0 +1,291 @@
+#redoing my allopatry script
+
+library(tidyverse)
+
+
+#Steps:
+# Goal: Create an adjacency matrix of degree of overlap
+# eq: area of intersect/area of smaller Fitzpatrick and Tulleri 2006
+
+#goal: Create a matrix of Euclidean distance between points
+# for each occurence i of species x
+#eq1: 0(xi) = w(xi)/b(xi) where w is ueclidian distance to nearest, and and b is distance to nearest heterospecif
+#for each species
+#eq2: p = n(0>1)/N(0)
+#where n(0>1) is number of 0 values in which the distance to conspecific is greater than hetero and
+#N(0) is total number of 0 values for species
+
+#eq3: Thus, Overlap of each species is 0xy = (p(x) + p(y))/2
+#0-1 bounded, where 0 is no overlap, and .5 is overlap is everything is random
+
+
+#First Step. Adjacency matrix of degree overlap
+# General Idea is to iterate over the SPDF and create degree overlap matrix for all species
+
+nrow(spec_poly)
+
+for (i in 1:10){
+  for(j in 1:10){
+
+    test <- raster::intersect(spec_poly[i,],spec_poly[j,])
+    plot(spec_poly[spdf$binomial == "Agalychnis_callidryas",], col = 'red')
+    plot(spec_poly[spec_poly$binomial == "Bolitoglossa_mexicana",], col = 'blue', add = TRUE)
+    plot(test, col = 'purple', add = TRUE)
+  }
+}
+
+
+test_ <- sf::st_intersects(spdf[spdf$binomial == "Agalychnis_callidryas",],
+                           spdf[spdf$binomial == "Bolitoglossa_mexicana",])
+test_
+test_ <- raster::intersect(spec_poly[spec_poly$binomial == "Agalychnis_callidryas",],
+                           spec_poly[spec_poly$binomial == "Bolitoglossa_mexicana",])
+
+
+rownames(inter_matrix[,1]) <- spec_poly[1,]$binomial
+intersect_mat <- as.matrix()
+inter_matrix <- data.frame()
+colnames(inter_matrix)[1]  <- "test"
+inter <- raster::crop(spec_poly[1,],spec_poly[1,])
+
+test <- as(spec_poly, "sf")
+(test[4,]$binomial)
+list(poly_test$binomial,poly_test$binomial)
+rownames(inter_matrix)[1]
+
+spdf <- as(poly_test,"sf")
+sf::st_crs(spdf) <- "+init=epsg:4326"
+
+area_over <- sum(sf::st_area(suppressWarnings(sf::st_intersection(spdf[1507,1], spdf[1517,1]))))
+plot(spdf[1517,3])
+lwgeom::lwgeom_make_valid(spdf[1507,])
+sf::st_
+
+x <- raster::raster(ncol = 5, nrow = 5, ext = raster::extent(poly_test), vals = 1)
+plot(x)
+plot(poly_test[5,], add = TRUE)
+
+
+inter <- sf::st_intersects(x_sf[,], spdf[])
+
+
+x_sf <- as(x, "SpatialPolygonsDataFrame")%>%
+  st_as_sf()
+
+
+Iterate_intersect <- function(spdf_in, raster_size = numeric()){
+
+  require('sf')
+  output_matrix <- matrix(nrow = 1, ncol = 1, dimnames = list("row","Column"))
+
+
+  #Create A raster that divides the data into easier chunks based on raster size
+  #This is a square raster
+  x <- raster::raster(ncol = raster_size,
+                      nrow = raster_size,
+                      ext = raster::extent(spdf_in),
+                      vals = 1)
+  #we then convert that into a sf object
+  x_sf <- as(x, "SpatialPolygonsDataFrame")%>%
+    st_as_sf()
+
+
+  #convert SPDF to sf
+  spdf_raw <- as(spdf_in,"sf")
+  sf::st_crs(spdf_raw) <- "+init=epsg:4326"
+  sf::st_crs(x_sf) <- "+init=epsg:4326"
+  #then we get which polygons are present in each spdf. this is a binary function
+  #that will output a list, where each item in the list is a vector containing the rows that
+  # from your sf object that are present in each grid cell
+  inter <- sf::st_intersects(x_sf[], spdf_raw[])
+
+  #Next we iterate through the list creating subsets of our spdf object for each grid cell
+  #then perform the matrix calculations. Note that this process sitll contains the geometry that
+  #goes outside the cells, but only contains geometries that intersect into each cell
+  length(inter)
+
+  #now so we dont do the same comparisons twice we will get a taxon list
+  taxon <- spdf_raw$binomial
+  taxon$ID <- rownames(taxon)
+
+
+  for(k in 1:length(inter)){
+    print(paste("working on section ", k))
+    spdf <- spdf_raw[inter[[k]],]
+
+    #intermatrix is intermediary output object
+    inter_matrix <- matrix(nrow = nrow(spdf), ncol = nrow(spdf),
+                           dimnames = list(spdf$binomial,spdf$binomial))
+
+
+    for(i in 1:(nrow(spdf)-1)){
+      print(paste(i,"/",nrow(spdf)))
+      for(j in (i+1):nrow(spdf)){
+        print(paste(j,"/",nrow(spdf)))
+        print(paste("now doing ",k,": ", i," x ",j))
+        if(sf::st_is_valid(spdf[i,1])){
+          if(sf::st_is_valid(spdf[j,1])){
+            area_over <- sum(sf::st_area(suppressWarnings(sf::st_intersection(spdf[i,1], spdf[j,1]))))
+            area_smaller <- min(sum(st_area(spdf[i,1])),sum(st_area(spdf[j,1])))
+
+            #area_larger <- max(st_area(spdf[i,1]),st_area(spdf[j,1]))}
+
+            if(length(area_over)==0){
+              area_over <- 0
+            }else{
+
+            }
+            print(area_over)
+            inter_matrix[i,j] <- area_over/area_smaller
+            rownames(inter_matrix)[i] <- spdf[i,]$binomial
+            colnames(inter_matrix)[j] <- spdf[j,]$binomial
+          }else{
+            print(paste('failed on', spdf[j,]$binomial))
+            inter_matrix[i,j] <- paste('failed on', spdf[j,]$binomial)
+            rownames(inter_matrix)[i] <- spdf[i,]$binomial
+            colnames(inter_matrix)[j] <- spdf[j,]$binomial
+          }
+        }else{
+          print(paste('failed on', spdf[i,]$binomial))
+          inter_matrix[i,j] <- paste('failed on', spdf[i,]$binomial)
+          rownames(inter_matrix)[i] <- spdf[i,]$binomial
+          colnames(inter_matrix)[j] <- spdf[j,]$binomial
+        }
+      }
+    }
+
+    output_matrix <- merge(output_matrix,inter_matrix, all = TRUE)
+    rownames(output_matrix)<- colnames(output_matrix)
+  }
+
+  return(output_matrix)
+}
+
+
+optimal_div <- function(spdf_in, min,max) {
+  require('sf')
+  output_mat <- matrix(ncol = 2, nrow = 1)
+  #convert SPDF to sf
+  spdf_raw <- as(spdf_in,"sf")
+  taxon <- spdf_in$binomial
+
+
+  for (i in min:max){
+    #Create A raster that divides the data into easier chunks based on raster size
+    #This is a square raster
+
+    check_mat <- matrix(ncol = length(taxon), nrow = length(taxon),
+                        dimnames = list(taxon,taxon))
+
+    x <- raster::raster(ncol = i,
+                        nrow = i,
+                        ext = raster::extent(spdf_in),
+                        vals = 1)
+    #we then convert that into a sf object
+    x_sf <- as(x, "SpatialPolygonsDataFrame")%>%
+      st_as_sf()
+
+    sf::st_crs(spdf_raw) <- "+init=epsg:4326"
+    sf::st_crs(x_sf) <- "+init=epsg:4326"
+    #then we get which polygons are present in each spdf. this is a binary function
+    #that will output a list, where each item in the list is a vector containing the rows that
+    # from your sf object that are present in each grid cell
+    inter <- sf::st_intersects(x_sf[], spdf_raw[])
+
+    totalcalc <- 0
+
+    for (j in 1:length(inter)){
+      n_calc = 0
+      if (length(inter[[j]]>0)){
+        for (k in 1:length(inter[[j]])){
+          for (l in k:length(inter[[j]])){
+            if (is.na(check_mat[inter[[j]][k],inter[[j]][l]])){
+              check_mat[inter[[j]][k],inter[[j]][l]] <- 1
+              n_calc = n_calc + 1
+            }
+          }
+        }
+        totalcalc <- totalcalc + n_calc
+      }else{
+        totalcalc = totalcalc
+      }
+    }
+
+    output_mat <- rbind(output_mat, c(i,totalcalc))
+
+  }
+  output_mat <- data.frame("Num_Div" = output_mat[-1,1],
+                           "Num_calc" = output_mat[-1,2])
+  return(list(output_mat,check_mat))
+}
+
+opt_div <- optimal_div(agg,1,50)
+
+
+check <- opt_div[[2]]
+opt_div <- opt_div[[1]]
+plot(opt_div[,1], opt_div[,2],
+     main = "Optimal number of pairwise calculation divisions",
+     xlab = "Number of divisions",
+     ylab = "Number of calculations to perform")
+
+arrows(x1 = opt_div[opt_div[,2] == min(opt_div[,2]),1],
+       y1 = min(opt_div[,2]), col = 'blue',
+       x0 = opt_div[opt_div[,2] == min(opt_div[,2]),1]-1,
+       y0 = min(opt_div[,2])-1)
+
+?arrows
+opt_div[opt_div[,2] == min(opt_div[,2]),1]
+min(opt_div[,2])
+out <- Iterate_intersect(agg,5)
+
+poly_test_sub <- subset(poly_test, poly_test@data$id_no %in% unique(poly_test@data[,c(1,2,7)]$binomial)$id_no)
+
+poly_test <- poly_test[order(poly_test$binomial,poly_test$year_),]
+
+agg <- raster::aggregate(poly_test,by = c('id_no','binomial','year_'), dissolve = TRUE)
+agg <- agg[order(agg$binomial,agg$year_, decreasing = TRUE),]
+agg@data$dupe <- duplicated(agg$binomial)
+agg <- agg[agg$dupe == FALSE,]
+
+merge()
+
+
+
+
+
+
+
+length(poly_test)
+
+for(j in 1:nrow()){
+  for(i in 1:nrow(spec_poly)){
+    print(c(i,":",j))
+    inter <- raster::crop(spec_poly[i,],spec_poly[j,])
+
+    if(is.null(inter)){
+      area <- 0
+    }else{
+      area <- sum(raster::area(inter))
+    }
+    print(area)
+    inter_matrix[i,j] <-  area
+    rownames(inter_matrix)[i] <- spec_poly[i,]$binomial
+    colnames(inter_matrix)[j] <- spec_poly[j,]$binomial
+    area ==0
+  }
+}
+
+rownames(inter_matrix)
+
+poly_test <- spec_poly_AS[spec_poly_AS$binomial %in% phylo_AS_raw$tip.label,]
+
+testing <- sf::st_intersection(test[1,1], test[1,1])
+sf::st_area(testing)
+apply(spec_poly[[]],1, function(x) print(x))
+
+test <- as(spec_poly, "sf")
+plot(test[2,1])
+
+test <- sf::st_as_sf(get(spec_poly[[1]]))
+colname
--- R/functions.R
+++ R/functions.R
@@ -3,25 +3,25 @@
 ###Cleaning RawGBIF Data####
 
 clean_gbif <- function(df, filter = NULL, resolveTaxonomy = FALSE, resolveID = NULL){
-  
+
   require(tidyverse)
   require(CoordinateCleaner)
   require(taxize)
   #filter by Genus
-  
+
   print(nrow(df))
   df <- df[,c("gbifID","basisOfRecord","decimalLatitude","decimalLongitude",
               "coordinateUncertaintyInMeters","coordinatePrecision",
               "scientificName",
               "class","order","family","genus","species")]
-  
+
   if(!is.null(filter)){
-    
+
     df <- filter(df, df$genus == filter)
-    
+
     print(nrow(df))
   }
-  
+
   #clean blank species
   df <- df %>%
     filter(!is.na(df$species))
@@ -31,21 +31,21 @@ clean_gbif <- function(df, filter = NULL, resolveTaxonomy = FALSE, resolveID = N
     filter(df$species != "")
   df <- df %>%
     filter(df$species != " ")
-  
+
   #remove blank coordinates
   df <- df %>%
     filter(.$decimalLatitude != ",")%>%
     filter(.$decimalLongitude != ",")
-  
+
   #remove records such as fossils
   df <- filter(df, df$basisOfRecord == "HUMAN_OBSERVATION" |
                  df$basisOfRecord == 'OBSERVATION' |
                  df$basisOfRecord == 'PRESERVED_SPECIMEN')
-  
+
   df <- filter(df, is.numeric(df$coordinateUncertaintyInMeters) <= 50000)
-  
+
   names(df)[3:4] <- c('decimallatitude', 'decimallongitude')
-  
+
   #clean using CoordinateCleaner
   df <- df %>%
     cc_val() %>%
@@ -61,17 +61,17 @@ clean_gbif <- function(df, filter = NULL, resolveTaxonomy = FALSE, resolveID = N
   #create taxon list
   taxon <- as.character(df$species)%>%
     unique()
-  
+
   if(resolveTaxonomy == TRUE){
     print("Now Resolving Taxonomy")
     #using taxize to clean the scientific names and merge
-    new_tax <- taxize::gnr_resolve(taxon, best_match_only = TRUE, 
+    new_tax <- taxize::gnr_resolve(taxon, best_match_only = TRUE,
                                    canonical = TRUE, http = 'post', data_source_ids = as.numeric(resolveID))
-    
+
     df <-merge(df,new_tax, by.x = 'species', by.y = 'user_supplied_name')
   }
 
-  
+
   return(df)
 }
 
@@ -81,29 +81,29 @@ KDE_filter <- function(points, unispecies = TRUE, low_r = .25, up_r = .75){
   require(spatstat)
   require(tidyverse)
   require(raster)
-  
+
   if(unispecies == TRUE){
     #points_clean_kde = data.frame(x,y)
 
     win <- extent(matrix(c(points$decimallongitude,points$decimallatitude), nrow = nrow(points)))
     win <- data.frame(c(win@xmin, win@xmax),
                       c(win@ymin, win@ymax))
-    
+
     win <- owin(c(win[1,1],win[2,1]),c(win[1,2],win[2,2]))
-    
+
     #create ppm
     gbif_ppp <- ppp(points$decimallongitude,points$decimallatitude, window = win)
-    
+
     #KDE
     gbif_kde <- density.ppp(gbif_ppp, at = "points")
     gbif_kde <- data.frame(gbif_kde, gbif_ppp$x,gbif_ppp$y)
-    
+
     #Exclude Outliers
     q <- quantile(gbif_kde$gbif_kde, probs = c(low_r,up_r))
     iqr <- IQR(gbif_kde$gbif_kde)
     uq <- q[1] + iqr
     lq <- q[2] - iqr
-    
+
     gbif_kde <- gbif_kde[gbif_kde$gbif_kde <= uq,]
     gbif_kde <- gbif_kde[gbif_kde$gbif_kde >= lq,]
 
@@ -118,9 +118,9 @@ KDE_filter <- function(points, unispecies = TRUE, low_r = .25, up_r = .75){
       print(i)
 
       points_ <- subset(points, points[,1] == levels(factor(points[,1]))[i])
-      
+
       if(nrow(points_)<=5){
- 
+
         print(paste("Removing ", points_[1,1], " due to fewer than 5 records"))
         points_merge <- data.frame('species' = points_[1,1],
                                    'x' = points_$decimallongitude, 'y'= points_$decimallatitude,
@@ -129,32 +129,32 @@ KDE_filter <- function(points, unispecies = TRUE, low_r = .25, up_r = .75){
         points_clean_kde <- rbind(points_clean_kde,points_merge)
         next
       }else{
-        
+
         win <- extent(matrix(c(points_$decimallongitude,points_$decimallatitude), nrow = nrow(points_)))
         win <- data.frame(c(win@xmin, win@xmax),
                           c(win@ymin, win@ymax))
         win <- owin(c(win[1,1],win[2,1]),c(win[1,2],win[2,2]))
-        
+
         #create ppm
         gbif_ppp <- ppp(points_$decimallongitude,points_$decimallatitude, window = win)
-        
+
         #KDE
         gbif_kde <- density.ppp(gbif_ppp, at = "points")
         gbif_kde <- data.frame(gbif_kde, gbif_ppp$x,gbif_ppp$y)
-        
+
         #Exclude Outliers
         q <- quantile(gbif_kde$gbif_kde, probs = c(low_r,up_r))
         iqr <- IQR(gbif_kde$gbif_kde)
         uq <- q[2] + (iqr * 1.5)
         lq <- q[1] - (iqr * 1.5)
-        
+
         gbif_kde <- gbif_kde[gbif_kde$gbif_kde <= uq,]
         gbif_kde <- gbif_kde[gbif_kde$gbif_kde >= lq,]
-        
+
         nrow(gbif_kde)
-        
+
         if(nrow(gbif_kde)<=1){
-     
+
           print(paste(points_[1,1], " removed following filtering "))
           points_merge <- data.frame('species' = points_[1,1],
                                      'x' = points_$decimallongitude, 'y'= points_$decimallatitude,
@@ -163,12 +163,12 @@ KDE_filter <- function(points, unispecies = TRUE, low_r = .25, up_r = .75){
           next
         }else{
           print(paste('Adding ',points_[1,1]))
-          
+
           points_merge <- data.frame('species' = points_[1,1],
                                      'x' = gbif_kde$gbif_ppp.x, 'y'= gbif_kde$gbif_ppp.y,
                                      "KDE_status" = "Filter_Successful")
           points_clean_kde <- rbind(points_clean_kde,points_merge)
-          
+
         }
       }
     }
@@ -181,7 +181,7 @@ KDE_filter <- function(points, unispecies = TRUE, low_r = .25, up_r = .75){
 # map created from map function (used to crop the circles to a landmass)
 
 
-points2Poly <- function(points,x = 'x', y = 'y', species = 'species', map, 
+points2Poly <- function(points,x = 'x', y = 'y', species = 'species', map,
                         alpha = FALSE, a = 1,
                         type = 'All', buffer_m = 1000){
   require(raster)
@@ -190,30 +190,30 @@ points2Poly <- function(points,x = 'x', y = 'y', species = 'species', map,
   require(rangeBuilder)
 
   spdf <- points
-  
+
   #create spatialpointsdataframe
-  
+
   coordinates(spdf) <- ~x+y
   projection(spdf) <- projection(map)
   spdf@data$species <- points$species
-  
+
   #filtering records that are not on map (some species may be removed)
-  
+
   spdf <- raster::intersect(spdf,map)
-  
+
   #make circles around each point. D is in meters. Convert to polygon
-  
+
   poly_list <- list()
   sp_list <- unique(spdf$species)
 
   for( i in 1:length(sp_list)){
     print(paste("Now doing", sp_list[i], i,"/",length(sp_list)))
-      pol <- polygons(dismo::circles(spdf[which(spdf@data$species == sp_list[i]),], 
+      pol <- polygons(dismo::circles(spdf[which(spdf@data$species == sp_list[i]),],
                                    d=buffer_m,lonlat = TRUE))
       projection(pol) <- projection(map)
       #pol <- raster::intersect(pol,map)
       pol <- raster::union(pol)
-    
+
     if(alpha == TRUE){
       poly_coords <- matrix(ncol = 2)
 
@@ -226,25 +226,25 @@ points2Poly <- function(points,x = 'x', y = 'y', species = 'species', map,
       pol <- intersect(pol[[1]], map)
       poly_list[i] <- pol
       poly_list[[i]]@polygons[[1]]@ID = as.character(i)
- 
+
     }
     else{}
     poly_list[i] <- pol
     poly_list[[i]]@polygons[[1]]@ID = as.character(i)
   }
-  
+
 joined = SpatialPolygons(lapply(poly_list, function(x){x@polygons[[1]]}))
 jdata = SpatialPolygonsDataFrame(Sr=joined, data=data.frame(ID = 1:length(sp_list),species = sp_list),FALSE)
-  
+
 return(jdata)
 }
 
 ###Create map####
 mapcreate <- function(country_names){
-  
+
   require(maps)
   require(maptools)
-  
+
 map <- maps::map('world',
                  regions = country_names, #c("Mexico",
                              #'Belize',
@@ -254,7 +254,7 @@ map <- maps::map('world',
                              #'Costa Rica',
                              #'Panama',
                              #'Nicaragua'),
-                 fill = TRUE, 
+                 fill = TRUE,
                  interior = FALSE)
 
 
@@ -267,44 +267,191 @@ return(map_sp)
 
 }
 
+###Checks what species intersect?###
+Iterate_intersect <- function(spdf_in, raster_size = numeric()){
 
-sp_list <- as.list()
-spdf <- spec_poly_done
-cunt <- map_sp
+  require('sf')
+  output_matrix <- matrix(nrow = 1, ncol = 1, dimnames = list("row","Column"))
 
-sp_list <- as.character(factor(spdf$binomial))
 
-crop_list <- list()
+  #Create A raster that divides the data into easier chunks based on raster size
+  #This is a square raster
+  x <- raster::raster(ncol = raster_size,
+                      nrow = raster_size,
+                      ext = raster::extent(spdf_in),
+                      vals = 1)
+  #we then convert that into a sf object
+  x_sf <- as(x, "SpatialPolygonsDataFrame")%>%
+    st_as_sf()
 
-for (i in 1:length(sp_list)){
+  #convert SPDF to sf
+  spdf_raw <- as(spdf_in,"sf")
+  sf::st_crs(spdf_raw) <- "+init=epsg:4326"
+  sf::st_crs(x_sf) <- "+init=epsg:4326"
 
-  sub <- subset(spdf,spdf$binomial == sp_list[i])
-  rast <- raster::raster(ext = extent(sub), res = 2)
-  cunt_crop <- raster::crop(cunt,rast)
-  crop_list[[i]] <- cunt_crop
+  #then we get which polygons are present in each spdf. this is a binary function
+  #that will output a list, where each item in the list is a vector containing the rows that
+  # from your sf object that are present in each grid cell
+  inter <- sf::st_intersects(x_sf[], spdf_raw[])
+
+  #Next we iterate through the list creating subsets of our spdf object for each grid cell
+  #then perform the matrix calculations. Note that this process sitll contains the geometry that
+  #goes outside the cells, but only contains geometries that intersect into each cell
+  length(inter)
+
+  #now so we dont do the same comparisons twice we will get a taxon list
+  taxon <- spdf_raw$binomial
+  taxon$ID <- rownames(taxon)
+
+
+  for(k in 1:length(inter)){
+    print(paste("working on section ", k))
+    spdf <- spdf_raw[inter[[k]],]
+
+    #intermatrix is intermediary output object
+    inter_matrix <- matrix(nrow = nrow(spdf), ncol = nrow(spdf),
+                           dimnames = list(spdf$binomial,spdf$binomial))
+
+
+    for(i in 1:(nrow(spdf)-1)){
+      print(paste(i,"/",nrow(spdf)))
+      for(j in (i+1):nrow(spdf)){
+        print(paste(j,"/",nrow(spdf)))
+        print(paste("now doing ",k,": ", i," x ",j))
+        if(sf::st_is_valid(spdf[i,1])){
+          if(sf::st_is_valid(spdf[j,1])){
+            area_over <- sum(sf::st_area(suppressWarnings(sf::st_intersection(spdf[i,1], spdf[j,1]))))
+            area_smaller <- min(sum(st_area(spdf[i,1])),sum(st_area(spdf[j,1])))
+
+            #area_larger <- max(st_area(spdf[i,1]),st_area(spdf[j,1]))}
+
+            if(length(area_over)==0){
+              area_over <- 0
+            }else{
+
+            }
+            print(area_over)
+            inter_matrix[i,j] <- area_over/area_smaller
+            rownames(inter_matrix)[i] <- spdf[i,]$binomial
+            colnames(inter_matrix)[j] <- spdf[j,]$binomial
+          }else{
+            print(paste('failed on', spdf[j,]$binomial))
+            inter_matrix[i,j] <- paste('failed on', spdf[j,]$binomial)
+            rownames(inter_matrix)[i] <- spdf[i,]$binomial
+            colnames(inter_matrix)[j] <- spdf[j,]$binomial
+          }
+        }else{
+          print(paste('failed on', spdf[i,]$binomial))
+          inter_matrix[i,j] <- paste('failed on', spdf[i,]$binomial)
+          rownames(inter_matrix)[i] <- spdf[i,]$binomial
+          colnames(inter_matrix)[j] <- spdf[j,]$binomial
+        }
+      }
+    }
+
+    output_matrix <- merge(output_matrix,inter_matrix, all = TRUE)
+    rownames(output_matrix)<- colnames(output_matrix)
+  }
+
+  return(output_matrix)
 }
-  
-names(crop_list) <- sp_list
-library(raster)
 
-map <- maps::map(database="world", fill = TRUE, interior = TRUE)
-IDs <- sapply(strsplit(map$names,":"),function(x) x[1])
+### Find optimal number of divisions for Allopatry ###
+optimal_div <- function(spdf_in, min,max) {
+  require('sf')
+  output_mat <- matrix(ncol = 2, nrow = 1)
+  #convert SPDF to sf
+  spdf_raw <- as(spdf_in,"sf")
+  taxon <- spdf_in$binomial
 
-map_sp <- maptools::map2SpatialPolygons(map, IDs = IDs)%>%
-  raster::aggregate(., dissolv = TRUE)
 
-library(tidyverse)
-setwd("C:/Users/Admin/OneDrive - San Diego State University (SDSU.EDU)/Research/Phyloregion_SDSU/Phyloregionalization_MA/Outputs")
-load("prephylo.data")
+  for (i in min:max){
+    #Create A raster that divides the data into easier chunks based on raster size
+    #This is a square raster
 
-plot(crop_list$`Agkistrodon piscivorus`)
-plot(subset(spec_poly_done,spec_poly_done$binomial == 'Agkistrodon piscivorus'),
-     col = 'red', add = TRUE)
-projection(spdf)
-projection(map_sp)
+    check_mat <- matrix(ncol = length(taxon), nrow = length(taxon),
+                        dimnames = list(taxon,taxon))
 
-extent(sub)
+    x <- raster::raster(ncol = i,
+                        nrow = i,
+                        ext = raster::extent(spdf_in),
+                        vals = 1)
+    #we then convert that into a sf object
+    x_sf <- as(x, "SpatialPolygonsDataFrame")%>%
+      st_as_sf()
 
-plot(map_sp)
-extent(map_sp)
+    sf::st_crs(spdf_raw) <- "+init=epsg:4326"
+    sf::st_crs(x_sf) <- "+init=epsg:4326"
+    #then we get which polygons are present in each spdf. this is a binary function
+    #that will output a list, where each item in the list is a vector containing the rows that
+    # from your sf object that are present in each grid cell
+    inter <- sf::st_intersects(x_sf[], spdf_raw[])
+
+    totalcalc <- 0
+
+    for (j in 1:length(inter)){
+      n_calc = 0
+      if (length(inter[[j]]>0)){
+        for (k in 1:length(inter[[j]])){
+          for (l in k:length(inter[[j]])){
+            if (is.na(check_mat[inter[[j]][k],inter[[j]][l]])){
+              check_mat[inter[[j]][k],inter[[j]][l]] <- 1
+              n_calc = n_calc + 1
+            }
+          }
+        }
+        totalcalc <- totalcalc + n_calc
+      }else{
+        totalcalc = totalcalc
+      }
+    }
+
+    output_mat <- rbind(output_mat, c(i,totalcalc))
+
+  }
+  output_mat <- data.frame("Num_Div" = output_mat[-1,1],
+                           "Num_calc" = output_mat[-1,2])
+  return(list(output_mat,check_mat))
+}
+
+
+### Creates list of where species intersect with what chunks####
+chunk_pres_ab <- function(spdf_in, raster_size = numeric(),species_spdf){
+
+  require('sf')
+  output_matrix <- matrix(nrow = 1, ncol = 1, dimnames = list("row","Column"))
+
+
+  #Create A raster that divides the data into easier chunks based on raster size
+  #This is a square raster
+  x <- raster::raster(ncol = raster_size,
+                      nrow = raster_size,
+                      ext = raster::extent(spdf_in),
+                      vals = 1)
+  #we then convert that into a sf object
+  x_sf <- as(x, "SpatialPolygonsDataFrame")%>%
+    st_as_sf()
+
+
+  #convert SPDF to sf
+  spdf_raw <- as(spdf_in,"sf")
+  sf::st_crs(spdf_raw) <- "+init=epsg:4326"
+  sf::st_crs(x_sf) <- "+init=epsg:4326"
+
+  #then we get which polygons are present in each spdf. this is a binary function
+  #that will output a list, where each item in the list is a vector containing the rows that
+  # from your sf object that are present in each grid cell
+  inter <- sf::st_intersects(x_sf[], spdf_raw[])
+
+  #Next we iterate through the list creating subsets of our spdf object for each grid cell
+  #then perform the matrix calculations. Note that this process sitll contains the geometry that
+  #goes outside the cells, but only contains geometries that intersect into each cell
+  length(inter)
+
+
+
+  sp_inter <- sf::st_intersects(x_sf[],spdf_species[])
+  return(list(inter,sp_inter))
+
+}
 
