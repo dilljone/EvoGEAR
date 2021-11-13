13916d0025d267c6aadf78ee0d4f9148cb52aad4
Dillon Jones <dilljone96@gmail.com>
2021-11-13 06:13:07 -0800

    Added an IUCN dataset for amphibians. Possibly fixed the GBIF dataset so that it can be called

--- R/data.R
+++ R/data.R
@@ -2,7 +2,7 @@
 #'
 #' A dataset containing the raw GBIF occurrence information for Central American Amphibians. Obtained via download from GBIF on 11/12/2021.
 #' @references  GBIF.org (12 November 2021) GBIF Occurrence Download https://doi.org/10.15468/dl.c8gwtr
-#'asdasdasd
+#'
 #' @format A data frame with 250524 rows and 50 columns
 #' \Describe{
 #'  \item{ID}{}
@@ -10,7 +10,7 @@
 #'
 #' }
 #'
-#' @usage data(gbif)
+#' @usage read.csv(system.file('extdata','gbif.csv.gz', package = "evogear"))
 #' @doctype data
 #'
 #' @examples
@@ -19,5 +19,14 @@
 #' paste0(nrow(gbif) - nrow(gbif_clean)," records were cleaned from the raw dataset")
 #'
 #' @source https://doi.org/10.15468/dl.c8gwtr
-#"inst/extdata/gbif.csv.gz"
+"gbif"
+
+#'IUCN Occurence dataset
 #'
+#'A dateset containing amphibian species in Central America. Downloaded from IUCN.
+#'Filtered based on species found in filtered GBIF dataset (IUCN <- IUCN_maps[which(IUCN_maps$binomial %in% pp$species),])
+#'
+#'@format A .shp file that should be loaded as a spdf with 437 elements with 27 variables
+#'@doctype doctype data
+#'@usage ReadOGR('inst/extdata/iucn.shp')
+'iucn'
