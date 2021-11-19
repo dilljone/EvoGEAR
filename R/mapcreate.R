5eb4c1ce9bfae6f83badd66c3a5222853a573371
Dillon Jones <61479895+dilljone@users.noreply.github.com>
2021-11-18 16:14:26 -0800

    I think I fixed it

--- R/mapcreate.R
+++ R/mapcreate.R
@@ -1,22 +1,16 @@
 #'creates a simple map based on supplied country name list
 #'
 #'@param country_names A vector of country names to use
-#'
 #'@export
+
+
 mapcreate <- function(country_names){
 
   require(maps)
   require(maptools)
 
   map <- maps::map('world',
-                   regions = country_names, #c("Mexico",
-                   #'Belize',
-                   #'Guatemala',
-                   #'Honduras',
-                   #'El Salvador',
-                   #'Costa Rica',
-                   #'Panama',
-                   #'Nicaragua'),
+                   regions = country_names,
                    fill = TRUE,
                    interior = FALSE)
 
