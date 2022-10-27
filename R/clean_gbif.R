344b44399c657b7943da72e2d71fde9c42750837
Dillon Jones <dilljone96@gmail.com>
2022-10-26 17:44:46 -0700

    Updated error in Clean_GBIF function. Also streamlined code into Tidy pipeline

--- R/clean_gbif.R
+++ R/clean_gbif.R
@@ -6,57 +6,49 @@
 #'@export
 clean_gbif <- function(df, filter = NULL){
 
-require(CoordinateCleaner)
-#filter by Genus
-
-print(nrow(df))
-df <- df[,c("gbifID","decimalLatitude","decimalLongitude",
-            "coordinateUncertaintyInMeters","coordinatePrecision",
-            "scientificName", "basisOfRecord",
-            "class","order","family","genus","species")]
-
-if(!is.null(filter)){
-
-  df <- filter(df, df$genus == filter)
-
+  require(CoordinateCleaner)
+  #filter by Genus
+  df <- GBIF_herps
   print(nrow(df))
-}
+  df <- df[,c("gbifID","decimalLatitude","decimalLongitude",
+              "coordinateUncertaintyInMeters","coordinatePrecision",
+              "scientificName", "basisOfRecord",
+              "class","order","family","genus","species")]
 
-#clean blank species
-df <- df %>%
-  filter(!is.na(df$species))
-df <- df%>%
-  filter(df$species!= ",")
-df <- df%>%
-  filter(df$species != "")
-df <- df %>%
-  filter(df$species != " ")
+  if(!is.null(filter)){
 
-#remove blank coordinates
-df <- df %>%
-  filter(.$decimalLatitude != ",")%>%
-  filter(.$decimalLongitude != ",")
+    df <- filter(df, df$genus == filter)
 
-#remove records such as fossils
-df <- filter(df, df$basisOfRecord == "HUMAN_OBSERVATION" |
-            df$basisOfRecord == 'OBSERVATION' |
-            df$basisOfRecord == 'PRESERVED_SPECIMEN')
+    print(nrow(df))
+  }
 
-df <- filter(df, is.numeric(df$coordinateUncertaintyInMeters) <= 50000)
 
-names(df)[3:4] <- c('decimallatitude', 'decimallongitude')
+  df <- df %>%
+    filter(!is.na(species))%>%
+    filter(species!= ",")%>%
+    filter(species != "")%>%
+    filter(species != " ")%>%
+    filter(decimalLatitude != ",")%>%
+    filter(decimalLongitude != ",")%>%
+    filter(basisOfRecord == "HUMAN_OBSERVATION" |
+             basisOfRecord == 'OBSERVATION' |
+             basisOfRecord == 'PRESERVED_SPECIMEN')
 
-#clean using CoordinateCleaner
-df <- df %>%
-  cc_val() %>%
-  cc_equ() %>%
-  cc_cap() %>%
-  cc_cen() %>%
-  cc_gbif() %>%
-  cc_inst() %>%
-  cc_sea() %>%
-  cc_zero() %>%
-  cc_dupl()
+  df <- filter(df, is.numeric(df$coordinateUncertaintyInMeters) <= 50000)
 
-return(df)
+  names(df)[2:3] <- c('decimallatitude', 'decimallongitude')
+
+  #clean using CoordinateCleaner
+  df <- df %>%
+    cc_val() %>%
+    cc_equ() %>%
+    cc_cap() %>%
+    cc_cen() %>%
+    cc_gbif() %>%
+    cc_inst() %>%
+    cc_sea() %>%
+    cc_zero() %>%
+    cc_dupl()
+
+  return(df)
 }
