bcfd53dacb0af4d7e74c4a24f13823a6b1cdb7eb
Dillon Jones <dilljone96@gmail.com>
2022-11-18 14:02:55 -0800

    Added script for visualzing ACR results in a shiny app. Needs major work before usable

--- R/sf_collapse.R
+++ R/sf_collapse.R
@@ -7,31 +7,34 @@
 #'@export
 #'
 
-sf_collapse <- function(sf, group_col) {
+sf_collapse <- function(sf, group_col){
   sf_use_s2(FALSE)
   sf[is.na(sf)] <- "NA"
 
-  func <- function(x) {
-    if (nrow(x[1]) == 1) {
-      paste0(x[[2]][1], " has only 1 polygon and will not be collapsed") %>% print()
+  func <- function(x){
 
-    } else{
-      paste0("Now collapsing polygons for ",
-             x[[2]][1],
-             ". There are ",
-             nrow(x[1]),
-             " polygons.") %>% print()
+
+    if(nrow(x[1]) == 1){
+
+      paste0(x[[2]][1], " has only 1 polygon and will not be collapsed")%>%print()
+
+    }else{
+
+      paste0("Now collapsing polygons for ",x[[2]][1],". There are ",nrow(x[1]), " polygons.")%>%print()
+
     }
 
-    suppressMessages(x %>% st_union() -> out)
+
+    suppressMessages(x%>%st_combine()%>%st_union()-> out)
 
     return(out)
+
 
   }
 
   #split by category and union it
   sf %>%
-    split(., f = .[[group_col]]) %>%
+    split(., f = .[[group_col]])%>%
     lapply(func) -> out
 
   #grab labels here
@@ -40,10 +43,10 @@ sf_collapse <- function(sf, group_col) {
   do.call(c, out) -> out
 
   #create sf dataframe
-  out <- st_as_sf(data.frame(labels, st_geometry(out)))
+  out <- st_as_sf(data.frame(labels,st_geometry(out)))
 
   #Add name as group_col input
   names(out) <- c(paste(group_col), "geometry")
 
-  return(out)
-}
+  return(out)}
+
---
+++ R/vis_acr_shiny.R
@@ -0,0 +1,115 @@
+#'vis_acr_shiny is meant to launch a shiny application that helps you visualize your Ancestral State Reconstructions
+#'
+#'@param phylo is an object of class phylo with data related to states and their corresponding nodes
+#'@param sf is an object of class sf that contains the states for visualization
+#'@param state_list is a list of states and their frequencies. It should be named according to your nodes. Each node, needs an entry in the state list. Each dataframe in the list, should only have 2 columns, named state and freq in that order
+#'
+#'@details For the phylo object, each node should have a corresponding list that represents the states and their frequencies.
+#' A common example output would be similar to that one provided by RASP. This list, should contain a single dataframe, with 2 columns.
+#' The first column is the state, and the second column is the frequency.
+#'
+#'
+#'
+#'@export
+#'
+
+#where I left off, need to supply the list. Need to visualize with pie charts n shit. And do a better job so that you can visualize your stuff
+
+set.seed(123456)
+phylo <- ape::rtree(n = 20)
+phylo$node = 1:39
+
+
+for(i in 1:39){
+
+  states[i] <- list(data.frame(state = sample(c("A",'B','C','D','AB','AC','AD','BC','BD','CD'),
+                                                 5, replace = FALSE),
+                                  freq = sample(c(1,2,3,4,5),5, replace = FALSE)/15))}
+
+g <- ggtree(phylo)
+
+phylo$states
+
+
+vis_acr_shiny <- function(phylo, sf, state_list){
+
+  require(shiny)
+  require(sf)
+  require(tidyverse)
+  require(plotly)
+  require(ggtree)
+
+  states_filt = ""
+
+  phylo <- ggtree(phylo)
+
+  ui <- fluidPage(
+
+    # Application title
+    title("ASE Visualizer"),
+    hr(),
+    selectInput('Node',"Node Number:",phylo$data$node),
+    hr(),
+    fluidRow(column(7,
+                    plotlyOutput("phylo")),
+
+             column(5,
+                    plotOutput('state'))
+    )
+  )
+
+  server <- function(input, output) {
+
+    output$phylo <- renderPlotly({
+
+      p2 <- p2 +
+        geom_point(aes(x = x,
+                       y = y,
+                       colour = states,
+                       label = node))
+
+      plotly::ggplotly(p2, tooltip = c('node','states'))
+
+    })
+
+    states <- reactive({
+
+      states_filt <- phylo$data$states[which(phylo$data$node == input$Node)]%>%
+        strsplit(., split = "")%>%
+        unlist()
+
+      reg <- st_drop_geometry(sf)
+
+      for(i in 1:nrow(reg)){
+
+        if(reg[i,"state"] %in% states_filt){
+
+          reg[i,"col"] = "Present"
+
+        }else{
+          reg[i,"col"] = "Not Present"}
+      }
+
+      sf$col = reg$col
+
+      p <- ggplot(data = sf)+
+        geom_sf(aes(fill = col))+
+        geom_sf_label(aes(label = state))
+
+      p
+
+      return(p)
+    })
+
+
+    output$state <- renderPlot({
+      states()
+
+    })
+
+
+  }
+
+  # Run the application
+  shinyApp(ui = ui, server = server)
+}
