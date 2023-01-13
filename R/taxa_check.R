811feb869e5656b3d2f6dcb38425f729056c85ab
Dillon Jones <61479895+dilljone@users.noreply.github.com>
2023-01-13 11:18:23 -0800

    added a quick subfunction to check the GBIF taxonomy backbone on request and return that to the taxa_check function

--- R/taxa_check.R
+++ R/taxa_check.R
@@ -4,7 +4,7 @@
 #'
 #'@param list a list of taxa from various datasets. Can be either dataframes or a list of vectors
 #'@param var_name NULL by default. If supplied datasets are in dataframe format, you can specify the variable names
-#'
+#'@param gbif_backbone Boolean. FALSE by default. Will query the GBIF backbone and supply a secondary list detailing
 #'
 #'This function returns a dataframe where each item in the list constitutes a single column
 #'The rownames contain a unieue vector of taxa found in all datasets
@@ -15,7 +15,7 @@
 
 
 
-taxa_check <- function(list, var_name = NULL) {
+taxa_check <- function(list, var_name = NULL, gbif_backbone = F) {
   #List is a list of named datasets
   #var names is a vector of the variable name used in order of each  element of the list
 
@@ -42,5 +42,24 @@ taxa_check <- function(list, var_name = NULL) {
       }else{ out[i,j] = FALSE}
     }
   }
-  return(out)
+
+  if(gbif_backbone == TRUE){
+
+    require(rgbif)
+    gbif <- rgbif::name_backbone_checklist(taxa_all)
+
+    out$gbif_status <- gbif$status
+    out$gbif_confidence <- gbif$confidence
+    out$gbif_rank <- gbif$rank
+
+    return(list(out,gbif))
+
+  }else{
+    return(out)
+
+  }
+
+
 }
+
+taxa_all <- c("Rana vaillanti", "Lithobates sylvaticus", 'iguana iguana')
