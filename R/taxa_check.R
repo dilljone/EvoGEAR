1e274caa84a3a5f77bece4ae7ccaabe0c9643230
Dillon Jones <61479895+dilljone@users.noreply.github.com>
2023-01-13 10:58:55 -0800

    added taxa_check function which checks taxa given a user supplied list

--- R/taxa_check.R
+++ R/taxa_check.R
@@ -1,29 +1,46 @@
-#' Checks taxa given a named list of taxa
-#' Use case would be to supply taxa from your phylogeny, occurence records, and known databases
-#' Eventually will attempt to add in already known taxa databases
-#' #need to peroperly add at a later date
+#'This function will check if taxa are present according to a user supplied list
+#'Can be used to check where taxa are being excluded, where there are no overlaps,
+#'or to validate taxa across a list
+#'
+#'@param list a list of taxa from various datasets. Can be either dataframes or a list of vectors
+#'@param var_name NULL by default. If supplied datasets are in dataframe format, you can specify the variable names
 #'
 #'
+#'This function returns a dataframe where each item in the list constitutes a single column
+#'The rownames contain a unieue vector of taxa found in all datasets
 #'
 #'
-#'
-#'
-#'
-#'
-#'
+#'@export
 
 
-taxa_check <- function(list = character()){
 
-  taxa <- unlist(list)%>%unique()
-  out <- data.frame(taxa = taxa)
 
-  for(i in 1:length(list)){
+taxa_check <- function(list, var_name = NULL) {
+  #List is a list of named datasets
+  #var names is a vector of the variable name used in order of each  element of the list
 
-    out[,i+1] <- out$taxa %in% list[[i]]
-
+  taxa_all <- vector()
+  if(is.null(var_name)){
+    for( i in 1:length(list)){
+      taxa_all <- c(taxa_all, list[[i]])
+    }
+  }else{
+    for( i in 1:length(list)){
+      taxa_all <- c(taxa_all, list[[i]][,var_name])
+    }
   }
+  #Keep only unique
+  taxa_all <- unique(taxa_all)%>%sort()
 
-  colnames(out) <- c("taxa", names(list))
+  out <- matrix(nrow = length(taxa_all), ncol = length(list))%>%as.data.frame()
+  rownames(out) <- taxa_all
+  colnames(out) <- names(list)
+  for(i in 1:nrow(out)){
+    for(j in 1:ncol(out)){
+      if(taxa_all[i] %in% list[[j]]){
+        out[i,j] = TRUE
+      }else{ out[i,j] = FALSE}
+    }
+  }
   return(out)
 }
